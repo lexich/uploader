988ed8b638b226eba8f95ef63d1983bd8da325c1
Efremov Alexey <lexich121@gmail.com>
2019-09-12 17:47:33 +0300

    Fix: test

--- src/routes/__tests__/routes.spec.ts
+++ src/routes/__tests__/routes.spec.ts
@@ -4,6 +4,7 @@ import { mock } from '../../args';
 import apiRoutes from '../api';
 import authRoutes from '../auth';
 import cheerio from 'cheerio';
+import * as path from 'path';
 import { setupMiddleware, setupErrorHandlers } from '../../app';
 function getAgent() {
   const app = express();
@@ -25,27 +26,6 @@ afterEach(() => {
   restore();
 });
 
-test('GET / page without auth shoud redirect to login page', async () => {
-  const res = await getAgent().get('/');
-  expect(res.redirect).toBeTruthy();
-  expect(res.header.location).toBe('/login');
-});
-
-test('GET /login page without auth should render template', async () => {
-  const res = await getAgent().get('/login');
-  expect(res.status).toBe(200);
-  const $ = cheerio.load(res.text);
-
-  const username = $('form input[name=username]');
-  expect(username.length).toBe(1);
-  const password = $('form input[name=password][type=password]');
-  expect(password.length).toBe(1);
-  const form = $(`form[method='POST'][action='/login']`);
-  expect(form.length).toBe(1);
-  const sumbit = $('form [type=submit]');
-  expect(sumbit.length).toBe(1);
-});
-
 async function login(
   agent: request.SuperTest<request.Test>,
   params = {
@@ -61,43 +41,112 @@ async function login(
   return { cookie, res };
 }
 
-test('POST /login with correct password and access to index page', async () => {
-  const agent = getAgent();
-  const { res, cookie } = await login(agent);
-  expect(res.redirect).toBeTruthy();
-  expect(res.header.location).toBe('/');
-  const resIndex = await agent.get('/').set('Cookie', cookie);
-  expect(resIndex.status).toBe(200);
-});
+describe('route "/login"', () => {
+  test('GET without auth should render template', async () => {
+    const res = await getAgent().get('/login');
+    expect(res.status).toBe(200);
+    const $ = cheerio.load(res.text);
 
-test('POST /login with incorrect password', async () => {
-  const agent = getAgent();
-  const { res } = await login(agent, {
-    username: 'test1',
-    password: 'test1'
+    const username = $('form input[name=username]');
+    expect(username.length).toBe(1);
+    const password = $('form input[name=password][type=password]');
+    expect(password.length).toBe(1);
+    const form = $(`form[method='POST'][action='/login']`);
+    expect(form.length).toBe(1);
+    const sumbit = $('form [type=submit]');
+    expect(sumbit.length).toBe(1);
   });
-  expect(res.status).toBe(401);
-  const $ = cheerio.load(res.text);
-  const username = $('form input[name=username]');
-  expect(username.val()).toBe('test1');
-  const message = $('form .text-danger');
-  expect(message.text()).toBe(`User test1 wasn't found`);
+
+  test('POST with correct password and access to index page', async () => {
+    const agent = getAgent();
+    const { res, cookie } = await login(agent);
+    expect(res.redirect).toBeTruthy();
+    expect(res.header.location).toBe('/');
+    const resIndex = await agent.get('/').set('Cookie', cookie);
+    expect(resIndex.status).toBe(200);
+  });
+
+  test('POST with incorrect password', async () => {
+    const agent = getAgent();
+    const { res } = await login(agent, {
+      username: 'test1',
+      password: 'test1'
+    });
+    expect(res.status).toBe(401);
+    const $ = cheerio.load(res.text);
+    const username = $('form input[name=username]');
+    expect(username.val()).toBe('test1');
+    const message = $('form .text-danger');
+    expect(message.text()).toBe(`User test1 wasn't found`);
+  });
 });
 
-test('GET /files without auth', async () => {
-  const res = await getAgent()
-    .get('/files')
-    .set('X-Requested-With', 'XMLHttpRequest');
-  expect(res.status).toBe(401);
+describe('route "/files"', () => {
+  test('GET without auth', async () => {
+    const res = await getAgent()
+      .get('/files')
+      .set('X-Requested-With', 'XMLHttpRequest');
+    expect(res.status).toBe(401);
+  });
+
+  test('GET with auth without content', async () => {
+    const agent = getAgent();
+    const { cookie } = await login(agent);
+    const res = await agent
+      .get('/files')
+      .set('Cookie', cookie)
+      .set('X-Requested-With', 'XMLHttpRequest');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual([]);
+  });
+
+  test('GET with auth with content', async () => {
+    mock(args => {
+      args.upload = path.join(__dirname, 'fixtures');
+    });
+    const agent = getAgent();
+    const { cookie } = await login(agent);
+    const res = await agent
+      .get('/files')
+      .set('Cookie', cookie)
+      .set('X-Requested-With', 'XMLHttpRequest');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual([
+      {
+        name: '1.txt',
+        url: '/media/test/1.txt'
+      }
+    ]);
+  });
 });
 
-test('GET /files with auth', async () => {
-  const agent = getAgent();
-  const { cookie } = await login(agent);
-  const res = await agent
-    .get('/files')
-    .set('Cookie', cookie)
-    .set('X-Requested-With', 'XMLHttpRequest');
-  expect(res.status).toBe(200);
-  expect(res.body).toEqual([]);
+describe('route "/"', () => {
+  test('GET without auth shoud redirect to login page', async () => {
+    const res = await getAgent().get('/');
+    expect(res.redirect).toBeTruthy();
+    expect(res.header.location).toBe('/login');
+  });
+
+  test('GET with auth shoud redirect to login page', async () => {
+    mock(args => {
+      args.upload = path.join(__dirname, 'fixtures');
+    });
+    const agent = getAgent();
+    const { cookie } = await login(agent);
+
+    const res = await agent.get('/').set('Cookie', cookie);
+    expect(res.status).toBe(200);
+    const $ = cheerio.load(res.text);
+    const script = $('script[init-dropzone]');
+    expect(script.length).toBe(1);
+    const content = script.html();
+    expect(content).toContain(
+      `var FILES = ${JSON.stringify([
+        {
+          url: '/media/test/1.txt',
+          name: '1.txt'
+        }
+      ])}`
+    );
+  });
 });
