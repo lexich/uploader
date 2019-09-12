c80e6c1a4e00fa4340ceb32a4c92e63583484afc
Efremov Alexey <lexich121@gmail.com>
2019-09-12 14:56:53 +0300

    Fix: login

--- src/routes/__tests__/routes.spec.ts
+++ src/routes/__tests__/routes.spec.ts
@@ -5,69 +5,75 @@ import apiRoutes from '../api';
 import authRoutes from '../auth';
 import cheerio from 'cheerio';
 import { setupMiddleware, setupErrorHandlers } from '../../app';
-import session from 'express-session';
-
 function getAgent() {
-    const app = express();
-    setupMiddleware(app, {
-        mockSessionOpts(opts) {
-            opts.store = new session.MemoryStore()
-            return opts;
-        }
-    });
-    app.use(apiRoutes());
-    app.use(authRoutes());
-    setupErrorHandlers(app);
-    return request.agent(app);
+  const app = express();
+  setupMiddleware(app);
+  app.use(apiRoutes());
+  app.use(authRoutes());
+  setupErrorHandlers(app);
+  return request.agent(app);
 }
 
 let restore: () => void;
 beforeEach(() => {
-    restore = mock(args => {
-        args.username = 'test';
-        args.password = 'test';
-    });
+  restore = mock(args => {
+    args.username = 'test';
+    args.password = 'test';
+  });
 });
 afterEach(() => {
-    restore();
+  restore();
 });
 
 test('GET / page without auth shoud redirect to login page', async () => {
-    const res = await getAgent().get('/');
-    expect(res.redirect).toBeTruthy();
-    expect(res.header.location).toBe('/login');
+  const res = await getAgent().get('/');
+  expect(res.redirect).toBeTruthy();
+  expect(res.header.location).toBe('/login');
 });
 
 test('GET /files page without auth shoud redirect to login page', async () => {
-    const res = await getAgent().get('/files');
-    expect(res.redirect).toBeTruthy();
-    expect(res.header.location).toBe('/login');
+  const res = await getAgent().get('/files');
+  expect(res.redirect).toBeTruthy();
+  expect(res.header.location).toBe('/login');
 });
 
 test('GET /login page without auth should render template', async () => {
-    const res = await getAgent().get('/login');
-    expect(res.status).toBe(200);
-    const $ = cheerio.load(res.text)
+  const res = await getAgent().get('/login');
+  expect(res.status).toBe(200);
+  const $ = cheerio.load(res.text);
 
-    const username = $('form input[name=username]');
-    expect(username.length).toBe(1);
-    const password = $('form input[name=password][type=password]');
-    expect(password.length).toBe(1);
-    const form = $(`form[method='POST'][action='/login']`)
-    expect(form.length).toBe(1);
-    const sumbit = $('form [type=submit]');
-    expect(sumbit.length).toBe(1)
+  const username = $('form input[name=username]');
+  expect(username.length).toBe(1);
+  const password = $('form input[name=password][type=password]');
+  expect(password.length).toBe(1);
+  const form = $(`form[method='POST'][action='/login']`);
+  expect(form.length).toBe(1);
+  const sumbit = $('form [type=submit]');
+  expect(sumbit.length).toBe(1);
 });
 
+async function login(
+  agent: request.SuperTest<request.Test>,
+  params = {
+    username: 'test',
+    password: 'test'
+  }
+) {
+  const res = await agent
+    .post('/login')
+    .send(params)
+    .set('Content-Type', 'application/x-www-form-urlencoded');
+  const cookie = (res as any).headers['set-cookie'];
+  return { cookie, res };
+}
 
 test('POST /login with correct password and access to index page', async () => {
-    const res = await getAgent().post('/login').send({
-        username: 'test',
-        password: 'test'
-    }).set('Content-Type', 'application/x-www-form-urlencoded');
-    expect(res.redirect).toBeTruthy();
-    expect(res.header.location).toBe('/');
-    const resIndex = await getAgent().get('/').set('Cookie', (res as any).headers['set-cookie']);
-    expect(resIndex.status).toBe(200);
-
+  const agent = getAgent();
+  const { res, cookie } = await login(agent);
+  expect(res.redirect).toBeTruthy();
+  expect(res.header.location).toBe('/');
+  const resIndex = await agent
+    .get('/')
+    .set('Cookie', (res as any).headers['set-cookie']);
+  expect(resIndex.status).toBe(200);
 });
